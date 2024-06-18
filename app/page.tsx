abdc37062b33a7453fd8f09ca245ebf5063a09d6
Joaquim Verges <joaquim.verges@gmail.com>
2024-06-18 21:53:43 +1200

    refactor: migrate to thirdweb

--- app/page.tsx
+++ app/page.tsx
@@ -1,76 +1,43 @@
 'use client';
 
-import {useSmartAccount} from '@biconomy/use-aa';
 import Balance from 'components/Balance';
-import Button from 'components/Button';
 import ContractRead from 'components/ContractRead';
 import ContractWrite from 'components/ContractWrite';
-import {shorten} from 'lib/utils';
-import {useAccount} from 'wagmi';
-
-import {usePrivy, useWallets} from '@privy-io/react-auth';
+import {chain, client} from 'components/providers';
+import {ConnectButton, useActiveAccount} from 'thirdweb/react';
 
 const MonoLabel = ({label}: {label: string}) => {
   return <span className="rounded-xl bg-slate-200 px-2 py-1 font-mono">{label}</span>;
 };
 
 export default function Home() {
-  // Privy hooks
-  const {ready, authenticated, login, logout} = usePrivy();
-  const {wallets, ready: walletsReady} = useWallets();
-
-  // WAGMI hooks
-  const {address} = useAccount();
-
-  // Biconomy hooks
-  const {smartAccountAddress} = useSmartAccount();
-
-  if (!ready) {
-    return null;
-  }
+  const account = useActiveAccount();
 
   return (
     <>
       <main className="min-h-screen bg-slate-200 p-4 text-slate-800">
         <div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
           <div className="border-1 flex flex-col items-start gap-2 rounded border border-black bg-slate-100 p-3">
-            <h1 className="text-4xl font-bold">Privy + WAGMI + Biconomy</h1>
-            {ready && !authenticated && (
+            <h1 className="mb-4 text-4xl font-bold">thirdweb</h1>
+            {!account && (
               <>
-                <p>You are not authenticated with Privy</p>
-                <div className="flex items-center gap-4">
-                  <Button onClick_={login} cta="Login with Privy" />
-                </div>
+                <p>You are not logged in</p>
               </>
             )}
-
-            {walletsReady &&
-              wallets.map((wallet) => {
-                return (
-                  <div
-                    key={wallet.address}
-                    className="flex min-w-full flex-row flex-wrap items-center justify-between gap-2 bg-slate-50 p-4"
-                  >
-                    <div>
-                      <MonoLabel label={shorten(wallet.address)} />
-                    </div>
-                  </div>
-                );
-              })}
-
-            {ready && authenticated && (
-              <>
-                <p className="mt-2">You are logged in with privy.</p>
-                <Button onClick_={logout} cta="Logout from Privy" />
-              </>
-            )}
-            {address && smartAccountAddress && (
+            <ConnectButton
+              client={client}
+              theme={'light'}
+              accountAbstraction={{
+                chain,
+                sponsorGas: true,
+              }}
+            />
+            {account && (
               <>
                 <h2 className="mt-6 text-2xl">Account</h2>
                 <p>
-                  address: <MonoLabel label={smartAccountAddress} />
+                  address: <MonoLabel label={account.address} />
                 </p>
-
                 <Balance />
                 <ContractRead />
                 <ContractWrite />
--- components/providers.tsx
+++ components/providers.tsx
@@ -1,65 +1,31 @@
 'use client';
 
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
-import {http} from 'viem';
-import {baseSepolia} from 'viem/chains';
+import { createThirdwebClient, getContract } from "thirdweb";
+import { baseSepolia } from "thirdweb/chains";
+import { ThirdwebProvider } from "thirdweb/react";
 
-import type {PrivyClientConfig} from '@privy-io/react-auth';
-import {PrivyProvider} from '@privy-io/react-auth';
-import {WagmiProvider, createConfig} from '@privy-io/wagmi';
-import { BiconomyProvider } from '@biconomy/use-aa';
+// API key
+const clientId =
+    process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID as string;
 
-const queryClient = new QueryClient();
+export const client = createThirdwebClient({
+  clientId,
+})
 
-// API keys
-const biconomyPaymasterApiKey =
-    process.env.NEXT_PUBLIC_PAYMASTER_API_KEY as string;
-const bundlerUrl = process.env.NEXT_PUBLIC_BUNDLER_URL as string;
-const alchemyApiKey = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY as string;
-const privyAuthUrl = process.env.NEXT_PUBLIC_PRIVY_AUTH_URL as string;
-const privyAppId = process.env.NEXT_PUBLIC_PRIVY_APP_ID as string;
+export const chain = baseSepolia;
 
-// greeter contract on base sepolia
-export const contractAddress = '0xC252d497C08AdCCDc6820EcA64856D332BE15c99';
-
-export const wagmiConfig = createConfig({
-  chains: [baseSepolia],
-  transports: {
-    [baseSepolia.id]: http(`https://base-sepolia.g.alchemy.com/v2/${alchemyApiKey}`),
-  },
+// greeter contract
+export const contract = getContract({
+  address: "0xC252d497C08AdCCDc6820EcA64856D332BE15c99",
+  chain,
+  client,
 });
 
-const privyConfig: PrivyClientConfig = {
-  embeddedWallets: {
-    createOnLogin: 'users-without-wallets',
-    requireUserPasswordOnCreate: true,
-    noPromptOnSignature: false,
-  },
-  loginMethods: ['wallet', 'email', 'google'],
-};
 
 export default function Providers({children}: {children: React.ReactNode}) {
   return (
-    <PrivyProvider
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      apiUrl={privyAuthUrl}
-      appId={privyAppId}
-      config={privyConfig}
-    >
-      <QueryClientProvider client={queryClient}>
-        <WagmiProvider config={wagmiConfig} reconnectOnMount={false}>
-        <BiconomyProvider
-          config={{
-            biconomyPaymasterApiKey,
-            bundlerUrl,
-          }}
-          queryClient={queryClient}
-        >
-          {children}
-          </BiconomyProvider>
-        </WagmiProvider>
-      </QueryClientProvider>
-    </PrivyProvider>
+    <ThirdwebProvider>
+      {children}
+    </ThirdwebProvider>
   );
 }
